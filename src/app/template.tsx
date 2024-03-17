aea7780b7af3699a1edb02b133b378746bc63bba
Izam Ghali <izamghali18@gmail.com>
2024-03-17 15:18:56 +0700

    STYLE: put Navbar and Footer on template.tsx, added @layer components on globals.css, & added shadow.png

---
+++ src/app/template.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer"
+
+export default function Template({ children }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <div className="relative">
+            <Navbar className='fixed z-10' />
+            { children }
+            <Footer />
+        </div>
+    )
+};
+
