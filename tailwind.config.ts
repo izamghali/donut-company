9f6ecd61619f572139cc98fc02721d44c52c2286
Izam Ghali <izamghali18@gmail.com>
2024-03-17 15:13:57 +0700

    INITIAL: first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,33 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'class',
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'dark': '#141924',
+        'accent': '#88AAEE'
+      },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [],
+    // themes: ["light", "dark"],
+    // themes: ["cupcake", "cmyk", "dark"],
+    // themes: ["light", "dark", "cupcake"],
+  },
 };
 export default config;
