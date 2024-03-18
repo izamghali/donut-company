e56ead83a0b1d4edc95c6725b6c3780c0ea68613
Izam Ghali <izamghali18@gmail.com>
2024-03-18 17:14:17 +0700

    COMPONENT: added Testimonials & its mock data. Added snap-y on Drawer

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -47,36 +47,37 @@ export default function Drawer({ className, menuData } : Props) {
             {/* // NOTE:  mt-16 is the length of the Navbar */}
             <div className="drawer-content flex flex-col mt-16 dark:bg-dark">
 
-                <h3 className="md:hidden block font-bold text-xl text-center">Our Menu</h3>
+                <h3 className="md:hidden block font-bold text-xl text-center dark:text-white">Our Menu</h3>
                 <div role="tablist" className="tabs 
-                    border-b-2 py-2 bg-white  md:hidden 
+                    border-b-2 border-x-0 py-2 bg-white  md:hidden 
                     flex justify-center
                     sticky top-[4rem] tabs-lifted mt-2
+                    dark:bg-dark dark:text-light z-30
                 ">
-                    <a role="tab" className="tab tab-active">Donuts</a>
+                    <a role="tab" className="tab snap-center tab-active">Donuts</a>
                     {/* content */}
-                    <a role="tab" className="tab hidden sm:block">Cookies & Brownies</a>
-                    <a role="tab" className="tab sm:hidden flex gap-2">
+                    <a role="tab" className="tab snap-center hidden sm:block">Cookies & Brownies</a>
+                    <a role="tab" className="tab snap-center sm:hidden flex gap-2">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cookie" viewBox="0 0 16 16">
                             <path d="M6 7.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m4.5.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3m-.5 3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
                             <path d="M8 0a7.96 7.96 0 0 0-4.075 1.114q-.245.102-.437.28A8 8 0 1 0 8 0m3.25 14.201a1.5 1.5 0 0 0-2.13.71A7 7 0 0 1 8 15a6.97 6.97 0 0 1-3.845-1.15 1.5 1.5 0 1 0-2.005-2.005A6.97 6.97 0 0 1 1 8c0-1.953.8-3.719 2.09-4.989a1.5 1.5 0 1 0 2.469-1.574A7 7 0 0 1 8 1c1.42 0 2.742.423 3.845 1.15a1.5 1.5 0 1 0 2.005 2.005A6.97 6.97 0 0 1 15 8c0 .596-.074 1.174-.214 1.727a1.5 1.5 0 1 0-1.025 2.25 7 7 0 0 1-2.51 2.224Z"/>
                         </svg>CnB
                     </a>
                     {/* content */}
-                    <a role="tab" className="tab">Beverage</a>
+                    <a role="tab" className="tab snap-center">Beverage</a>
                     {/* content */}
                 </div>
 
-                <div className=" flex flex-wrap xl:gap-8 gap-4 justify-center lg:justify-start p-4 md:p-10 overflow-y-scroll">
+                <div className="snap-y snap-mandatory lg:snap-none flex flex-wrap xl:gap-8 gap-4 justify-center lg:justify-start p-4 md:p-10 overflow-y-scroll">
                     {
                         // menuCards()
                         menuData.map((item, idx) => {
-                            return <div className="
+                            return <div className="snap-center
                                 card group relative 
                                 w-96 md:w-full lg:w-96
                                 min-w-[18rem] lg:max-w-[20rem] max-w-auto
                                 bg-base-100 shadow-xl overflow-hidden 
-                                text-dark  std-duration " 
+                                text-dark std-duration " 
                                 key={idx} >
                                 <img className="h-full cursor-zoom-in  group-hover:scale-105 std-duration" src={item.src} alt="" />
 
@@ -104,7 +105,7 @@ export default function Drawer({ className, menuData } : Props) {
                 </div>
             </div> 
             
-            <div className="drawer-side border-r-2">
+            <div className="drawer-side border-r-2 dark:border-r-0">
                 <label htmlFor="product-drawer" aria-label="open sidebar" className="drawer-overlay"></label>
                 <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content hidden md:block">
                 {/* Sidebar content here */}
@@ -116,8 +117,6 @@ export default function Drawer({ className, menuData } : Props) {
                         <a className=" p-2 cursor-pointer">Cookies & Brownies</a>
                         <a className=" p-2 cursor-pointer">Beverage</a>
                     </div>
-
-                
                 </ul>
             </div>
         </div>
---
+++ src/components/Testimonials.tsx
@@ -0,0 +1,94 @@
+import React from "react"
+
+export default function Testimonials() {
+
+    let tests = [
+        {
+          name: 'Michael Scott',
+          job: 'Manager',
+          address: 'USA',
+          testi: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus pariatur fugit quae aliquid. Tenetur error nihil optio magni blanditiis consequatur! '
+        },
+        {
+          name: 'Tuco Salamanca',
+          job: 'Sales',
+          address: 'MEX',
+          testi: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente, unde.'
+        },
+        {
+          name: 'Angela Sasono',
+          job: 'Accountant',
+          address: 'INA',
+          testi: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi rerum saepe, sed velit est pariatur!'
+        },
+    ]
+    
+    function testCards() {
+      return tests.map((item, idx) => {
+          return <div key={idx} className="w-[1px]">
+            <div className="card w-80 bg-base-100 shadow-xl border-2">
+              <div className="relative ">
+                  <svg className="absolute w-[1px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+                    <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388q0-.527.062-1.054.093-.558.31-.992t.559-.683q.34-.279.868-.279V3q-.868 0-1.52.372a3.3 3.3 0 0 0-1.085.992 4.9 4.9 0 0 0-.62 1.458A7.7 7.7 0 0 0 9 7.558V11a1 1 0 0 0 1 1zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612q0-.527.062-1.054.094-.558.31-.992.217-.434.559-.683.34-.279.868-.279V3q-.868 0-1.52.372a3.3 3.3 0 0 0-1.085.992 4.9 4.9 0 0 0-.62 1.458A7.7 7.7 0 0 0 3 7.558V11a1 1 0 0 0 1 1z"/>
+                  </svg>
+              </div>
+              <div className="card-body">
+                  <p>{item.testi}</p>
+                  <h2 className="card-title">
+                  {item.name}
+                  {/* <div className="badge badge-secondary">NEW</div> */}
+                  </h2>
+                  
+                  {/* <div className="card-actions justify-end">
+                  <div className="badge badge-outline">Fashion</div> 
+                  <div className="badge badge-outline">Products</div>
+                  </div> */}
+              </div>
+
+            </div>
+          </div>
+      })
+    }
+
+    return (
+        <section className="pt-24 pb-8">
+        
+            <h2 className="text-center">Satisfied Customers</h2>
+
+            <div className="flex gap-10 justify-center flex-wrap ">
+                
+              {
+                tests.map((item, idx) => {
+                  return <div className="relative" key={idx}>
+                  
+                    {/* card */}
+                    <div className="card min-h-[19rem] w-80 bg-base-100 shadow-xl z-50">
+
+                      <div className="card-body">
+                        <div className="relative">
+                          <svg className="bi bi-quote w-14 absolute -top-12" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                            <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388q0-.527.062-1.054.093-.558.31-.992t.559-.683q.34-.279.868-.279V3q-.868 0-1.52.372a3.3 3.3 0 0 0-1.085.992 4.9 4.9 0 0 0-.62 1.458A7.7 7.7 0 0 0 9 7.558V11a1 1 0 0 0 1 1zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612q0-.527.062-1.054.094-.558.31-.992.217-.434.559-.683.34-.279.868-.279V3q-.868 0-1.52.372a3.3 3.3 0 0 0-1.085.992 4.9 4.9 0 0 0-.62 1.458A7.7 7.7 0 0 0 3 7.558V11a1 1 0 0 0 1 1z"/>
+                          </svg>
+                        </div>
+                        <p>{item.testi}</p>
+                        <h3 className="card-title">
+                          {item.name}
+                        </h3>
+                        <div className="card-actions justify-start">
+                          <div className="badge badge-outline">{item.job}</div> 
+                          <div className="badge badge-outline">@ {item.address}</div>
+                        </div>
+                      </div>
+                      
+                    </div>
+                    
+                  </div>
+                })
+              }
+
+            </div>
+
+        </section>
+    )
+};
+
