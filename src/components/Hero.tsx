05193ecd653bcb6206948288211e503443269a12
Izam Ghali <izamghali18@gmail.com>
2024-03-21 17:44:54 +0700

    3D: removed donut 3D object from Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,5 @@
 "use client"
-import React, { useState } from "react"
-import { Donut } from "./Donut"
-import { Vector3 } from "three";
+import React from "react"
 import Link from "next/link";
 import Logo from "./Logo";
 import Image from "next/image";
@@ -10,34 +8,14 @@ import HeroVertical from '../../public/hero/hero-vertical.jpg'
 
 export default function Hero() {
     
-
-    return ( // NOTE:  add CTA btn to order
-            // NOTE:  h-full when using img, h-screen when displaying 3D
-            // NOTE:  pt-16 is the Navbar length
-            // NOTE:  section className when 3D : bg-accent dark:bg-dark h-screen relative flex items-center
+    return (
         <section className="relative flex items-center">
-            {/* <Donut 
-                className={`
-                    h-full
-                    w-screen lg:w-[30rem]
-                    absolute right-0 lg:right-[1rem] xl:right-[10rem]
-                    lg:std-duration
-                `}
-            /> */}
 
             <div className="h-screen md:h-auto">
-                <Image priority className="hidden md:block w-screen lg:h-screen object-cover" src={HeroHorizontal} alt="" />
+                <Image className="hidden md:block w-screen lg:h-screen object-cover" src={HeroHorizontal} alt="" />
                 <Image priority className="block md:hidden object-cover sm:w-full w-auto min-h-screen sm:h-auto" src={HeroVertical} alt="" />
             </div>
 
-            {/* NOTE:  hero content className when 3D
-                w-full lg:w-fit 
-                lg:absolute 
-                left-4 lg:left-10
-                bottom-[20rem] lg:bottom-[30rem] 
-                translate-y-52 md:translate-y-36 lg:translate-y-[8rem]
-                std-duration text-white text-center lg:text-start
-            */}
             <div className="
                 absolute m-auto left-0 right-0 
                 md:left-10
@@ -47,13 +25,10 @@ export default function Hero() {
                 ">
                 <p className="mb-6 hidden lg:block font-light tracking-wider text-dark md:translate-x-[17rem] w-fit">Gyms are cool, but have you tried</p>
                 <Logo 
-                    classNameH1="font-bold 
-                        text-5xl md:text-6xl lg:text-8xl
-                        " 
+                    classNameH1="font-bold text-5xl md:text-6xl lg:text-8xl" 
                     classNameSVG="fill-white w-10 md:w-20 lg:w-24" 
                 />
 
-                {/* <p className="text-2xl lg:text-5xl font-extralight mt-2">Sit | Eat | Repeat</p> */}
                 <p className="
                     text-xl md:text-2xl lg:text-4xl 
                     font-extralight mt-2 text-slate-100
@@ -68,7 +43,6 @@ export default function Hero() {
                             <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
                         </svg>
                     </Link>
-
                 </div>
 
 
