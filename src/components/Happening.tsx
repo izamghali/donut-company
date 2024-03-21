7204a68921bf56a0e0c46ecd01b8cd9dd3bb2ddf
Izam Ghali <izamghali18@gmail.com>
2024-03-21 12:36:42 +0700

    COMPONENT: changed remote url to local src for Happening image & used Image from next

--- src/components/Happening.tsx
+++ src/components/Happening.tsx
@@ -1,18 +1,18 @@
 'use client'
 import React from "react"
 import { Silkscreen } from "next/font/google"
+import Image, { StaticImageData } from "next/image"
+import fest1 from '../../public/festivities/fest-1.avif'
+import fest2 from '../../public/festivities/fest-2.avif'
+import fest3 from '../../public/festivities/fest-3.avif'
+import fest4 from '../../public/festivities/fest-4.avif'
+import fest5 from '../../public/festivities/fest-5.avif'
 
 const silkscreen = Silkscreen({subsets: ["latin"], weight: '400' })
 
 export default function Happening() {
 
-    let carousels = [
-        'https://images.unsplash.com/photo-1524646349956-1590eacfa324?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        'https://images.unsplash.com/photo-1595239094789-4e00e532528a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        'https://images.unsplash.com/uploads/141148589884100082977/a816dbd7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        'https://images.unsplash.com/photo-1610768798760-e01a4b43814e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        'https://images.unsplash.com/photo-1628630500614-1c8924c99c3e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-    ]
+    let carousels = [ fest1, fest2, fest3, fest4, fest5]
 
     let nums = Array.from(Array(20).keys())
 
@@ -29,7 +29,7 @@ export default function Happening() {
             </div>
 
             <div className="z-20 sticky top-[15rem] left-10 sm:left-[12rem] md:left-[15rem] lg:left-[20rem] h-fit std-duration">
-                <span className="font-medium text-3xl absolute bg-red-400 text-dark w-60">Tired of eating?</span>
+                <span className="font-medium text-3xl absolute text-dark w-60">Tired of eating?</span>
                 <button className="btn translate-y-[10rem] translate-x-[8rem] md:translate-x-[20rem] px-10 bg-std-yellow border-none hover:bg-std-yellow hover:translate-y-[9.5rem]">Join</button>
             </div>
 
@@ -85,12 +85,12 @@ export default function Happening() {
             {/* carousels */}
             <div className="top-0 -left-36 translate-x-[50rem] flex space-x-[-13rem] z-10 ">
                     
-                    { carousels.map((item, idx) => {
+                    { carousels.map((item : StaticImageData, idx: number) => {
                         return <div className={`w-[40rem] overflow-hidden hover:blur-0 duration-700 
                                 ${ idx !== carousels.length - 1 ? 'rotate-[82deg] z-10' : '' }
                                 ${ idx % 2 === 0 ? 'blur-[0.5px]': '' } 
                                 ${ idx === carousels.length - 2 ? 'z-20' : '' }`} key={idx}>
-                            <img
+                            <Image
                                 className={
                                     `w-full h-full hover:scale-105 duration-300 scale-[102%]
                                     ${ idx !== carousels.length - 1 ? '-rotate-[82deg]' : '' } `} 
